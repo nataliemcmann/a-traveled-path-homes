18a9eee08577c926717b5c5a385f17c50a7b6906
Natalie McMann <nmcmann110@gmail.com>
2023-03-07 16:57:52 -0600

    added profile fetchProfile saga, added dispatch and profileReducer and useEffect to ProfilePage

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import ProfileForm from '../ProfileForm/ProfileForm';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function ProfilePage () {
+    const dispatch = useDispatch();
+
+    const profileReducer = useSelector(store => store.profileReducer);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_Profile' });
+    }, []);
+
     return (
         <>
         <h2>This is the profile page</h2>
