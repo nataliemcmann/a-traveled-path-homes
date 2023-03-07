e35cf914e7351851db0772edf6c858864b0090ce
Natalie McMann <nmcmann110@gmail.com>
2023-03-07 11:56:47 -0600

    created ProfileForm and ProfilePage components. added ProfileForm route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import ProfileForm from '../ProfileForm/ProfileForm';
 
 import './App.css';
 
@@ -58,6 +59,14 @@ function App() {
             path="/user"
           >
             <UserPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            // logged in shows UserPage else shows LoginPage
+            exact
+            path= "/profileForm"
+          >
+            <ProfileForm />
           </ProtectedRoute>
 
           <ProtectedRoute
---
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+//mui components
+
+//css style
+
+function ProfileForm() {
+    return (
+        <>
+            <form>
+                <h2>This is the Profile Form</h2>
+            </form> 
+        </>    
+    )
+}
+
+export default ProfileForm;
