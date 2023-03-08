67c307847ca6de4cf2e42fe6e8dfd852212becb1
Natalie McMann <nmcmann110@gmail.com>
2023-03-08 14:02:32 -0600

    added a photo upload component

---
+++ src/components/PropertyPhotoUpload/PropertyPhotoUpload.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+//mui components
+import { Button, TextField } from '@mui/material';
+
+function PropertyPhotoUpload() {
+    const dispatch = useDispatch();
+
+
+    const [fileInputs, setFileInputs] = useState([]);
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default PropertyPhotoUpload;
