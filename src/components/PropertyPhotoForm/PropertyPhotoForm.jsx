6b650092745a83ef93ada7e4332e5da8ea20f749
Natalie McMann <nmcmann110@gmail.com>
2023-03-08 14:20:41 -0600

    made a photo upload interface

--- src/components/PropertyPhotoForm/PropertyPhotoForm.jsx
+++ src/components/PropertyPhotoForm/PropertyPhotoForm.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import './PropertyPhotoForm.css';
 //mui components
-import { Button, TextField } from '@mui/material';
+import { Card, CardHeader, CardContent, Button } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 function PropertyPhotoForm() {
     const dispatch = useDispatch();
@@ -9,8 +11,32 @@ function PropertyPhotoForm() {
 
     const [fileInputs, setFileInputs] = useState([]);
 
+    const onFileChange = (event) => {
+    
+    // Update the state
+    setFileInputs({ selectedFile: event.target.files });
+    
+    };
+
     return (
         <>
+            <Card>
+                <CardHeader 
+                title="Photos"
+                subheader="Upload photos of your property! 
+                You need at least five photos, but the more the merrier. 
+                Make it feel like home!"
+                />
+                <CardContent>
+                    <div className="uploadContainer">
+                        <Button component="label" onChange={onFileChange}   
+                        sx={{ color: '#111856'}}>
+                        <AddCircleOutlineIcon/> <input type="file" hidden />
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+
         </>
     )
 }
