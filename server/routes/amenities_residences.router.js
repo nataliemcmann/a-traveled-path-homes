11f9441f8620d042ef55b48cd92285caf8dca33c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-03-10 14:54:21 -0600

    Farther on both sagas and reducers for amenities

--- server/routes/amenities_residences.router.js
+++ server/routes/amenities_residences.router.js
@@ -6,17 +6,17 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 
 router.post('/', rejectUnauthenticated, (req, res) => {
-  const sqlValues = [req.body.amenitiesId, req.body.residenceId];
-  const sqlQuery = `
-  INSERT INTO "amenities_residences"
-      ("amenitiesId", "residenceId")
-  VALUES
-      ($1, $2);
-  `;
-  pool.query(sqlQuery, sqlValues)
-  .then(() => res.sendStatus(201))
-  .catch((err) => {
-      console.log('Task-user relation post failed: ', err);
-      res.sendStatus(500);
-  })
+const sqlValues = [req.body.amenitiesId, req.body.residenceId];
+const sqlQuery = `
+    INSERT INTO "amenities_residences"
+        ("amenitiesId", "residenceId")
+    VALUES
+        ($1, $2);
+    `;
+    pool.query(sqlQuery, sqlValues)
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+    console.log('Task-user relation post failed: ', err);
+    res.sendStatus(500);
+    })
 })
--- src/redux/sagas/amenities.saga.js
+++ src/redux/sagas/amenities.saga.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+function* fetchAmenities() {
+  try {
+
+      const response = yield axios.get('/api/amenities');
+
+      yield put({ type: 'SET_AMENITIES', payload: response.data });
+  } catch (error) {
+      console.log('could not find', error);
+  }
+}
 
 
 
