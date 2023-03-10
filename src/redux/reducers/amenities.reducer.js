0ecc9d1fee483d0d557ca6727108ff555dbd2012
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-03-10 11:36:51 -0600

    adding second saga for the amenities of a certain residence

--- src/redux/reducers/amenities.reducer.js
+++ src/redux/reducers/amenities.reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 const amenitiesArray = (state = [], action) => {
   switch (action.type) {
-    case 'SET_AMENITIES_ARRAY':
+    case 'SET_AMENITIES':
       return action.payload;
     default:
       return state;
