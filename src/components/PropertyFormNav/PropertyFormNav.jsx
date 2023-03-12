cda78d780451cdc52c797c02053613c6ccda59e0
Natalie McMann <nmcmann110@gmail.com>
2023-03-12 12:02:13 -0500

    created PropertyFormNav component files

---
+++ src/components/PropertyFormNav/PropertyFormNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './PropertyFormNav.css';
+//mui components
+
+function PropertyFormNav(props) {
+    const history = useHistory();
+
+
+
+    return (
+        <>
+            
+        </>
+    )
+}
