8d89d0a2d1019850410af3653871ad2d9acbeeb0
Natalie McMann <nmcmann110@gmail.com>
2023-03-14 16:42:27 -0500

    took excess code out of review page

--- src/components/PropertyReview/PropertyReview.jsx
+++ src/components/PropertyReview/PropertyReview.jsx
@@ -1,54 +1,32 @@
 import React from "react";
 import './Review.css'
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import { Card } from "@mui/material";
+import { Stack, Grid, Card, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import propertyReducer from "../../redux/reducers/residence.reducer";
-import residenceSaga from "../../redux/sagas/residence.saga";
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "@mui/material";
 import PropertyFormNav from "../PropertyFormNav/PropertyFormNav";
 
 function PropertyReview (){
     const history = useHistory()
     const dispatch = useDispatch();
+
     const propertyReducer = useSelector((store) => store.propertyReducer);
-   
-    
 
     const handleSubmit = (event) => {
         event.preventDefault();
     history.push('/ownerdashboard')
 }
 
-
-
     return (
     <>
     <PropertyFormNav className="review"/>
-    <Card sx={{textAlign: "center",
-                marginLeft: 30,
-                marginRight: 30,
-                marginBottom: 10,
-                fontSize: 20,
-                padding: 5,
-                wordSpacing: 2}}>
-        <h2> Review</h2>
-        <p> Dust the shelves and clean the windows. Does everthing look like it should? <br>
-        </br>This is what renters will see when looking at ypur property page. </p>
-       
-        <Box sx={{ width: '100%' }}>
-           {/* {residenceSaga.map(( id))} */}
-           <div> 
-         
-                <h2> Price: $3,000</h2>
-           </div>
-    </Box>
-    </Card>
+    <Stack>
+        <div>
+            <h1> Review</h1>
+            <p> Dust the shelves and clean the windows. Does everthing look like it should? </p>
+            <p> This is what renters will see when looking at ypur property page. </p>
+        </div> 
+    </Stack>
     <Button onClick={handleSubmit} type="submit"
                         size='large'
                         sx={{
