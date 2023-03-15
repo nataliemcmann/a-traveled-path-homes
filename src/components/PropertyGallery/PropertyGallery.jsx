723700846412f2758fa8709ef99f3417ab11a860
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-03-15 18:30:00 -0500

    property gallery created and adding photos to database

--- src/components/PropertyGallery/PropertyGallery.jsx
+++ src/components/PropertyGallery/PropertyGallery.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, Grid } from '@mui/material';
+import ResidencesItem from '../ResidencesItem/ResidencesItem';
 
 function PropertyGallery() {
   const history = useHistory();
@@ -14,7 +15,25 @@ function PropertyGallery() {
 }, [dispatch]);
 
   return(
+    <div>
     <h1>All Properties</h1>
+    <>
+      <Grid sx={{ 
+            display: 'grid',
+            gap: 3,
+            gridTemplateColumns: 'repeat(4, 2fr)',
+            padding: 1,
+            elevation: 8,
+            marginLeft: 6,
+            marginRight: 6
+            }}>
+              
+              {residencesReducer?.map(residences => (
+                <ResidencesItem key={residences.id} residences={residences} />
+            ))}
+        </Grid>
+        </>
+        </div>
   )
 
 }
