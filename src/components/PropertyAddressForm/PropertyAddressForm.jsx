2bfbfd54624e4fa3c6735d9d63f8b77cb5f87632
Natalie McMann <nmcmann110@gmail.com>
2023-03-15 14:33:48 -0500

    address form css updated

--- src/components/PropertyAddressForm/PropertyAddressForm.jsx
+++ src/components/PropertyAddressForm/PropertyAddressForm.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useDispatch, useSelector} from "react-redux"
 import { useHistory } from "react-router-dom";
 import PropertyFormNav from "../PropertyFormNav/PropertyFormNav";
+import './AddressForm.css';
 //mui components
 import { Stack, TextField, Button } from '@mui/material';
 
@@ -15,19 +16,22 @@ const ProperyAddressForm = () => {
 
     function handleAddressSubmit() {
         event.preventDefault();
-        let addressAsString = ''
-        if (address.apartment) {
-          addressAsString = `${address.street} ${address.apartment} ${address.city} ${address.state} ${address.zip}`;
-        } else {
-          addressAsString = `${address.street} ${address.city} ${address.state} ${address.zip}`;
-        }
-        console.log(addressAsString);
-        dispatch({type: 'SET_ADDRESS',
-        payload: addressAsString
-        })
-        history.push('/basics');
+        // let addressAsString = ''
+        // if (address.apartment) {
+        //   addressAsString = `${address.street} ${address.apartment} ${address.city} ${address.state} ${address.zip}`;
+        // } else {
+        //   addressAsString = `${address.street} ${address.city} ${address.state} ${address.zip}`;
+        // }
+        // console.log(addressAsString);
+        // dispatch({type: 'SET_ADDRESS',
+        // payload: addressAsString
+        // })
+        // history.push('/basics');
       }
 
+      const nextBtn = () => {
+        history.push('/basics');
+      }
       const cancelBtn = () => {
         history.push(`/ownerdashboard`)
     }
@@ -38,91 +42,98 @@ const ProperyAddressForm = () => {
         <div className="propertyAddress">
             <form onSubmit={handleAddressSubmit}>
             <Stack alignItems='center'>
-                  <h1> Address </h1>
-                  <p>Your address and location are safe with us. It won't be viewable by renders until you chose to list property publicly</p>
-                  <div>
-                    <label> Address
-                      <input 
-                      type="text" 
-                      value= {address.street || ''}
-                      onChange = {(event) => {
-                      dispatch({type: 'SET_STREET', payload: event.target.value})
-                      }}
-                      />
-                  </label>
-                  </div>
+              <div className="addressHeader">
+                <h1> Address </h1>
+                <p>Your address and location are safe with us. It won't be viewable by renders until you chose to list property publicly</p>
+              </div>  
+              <div className="addressFields">
+              <p>Address</p>
+                <TextField 
+                variant="outlined"
+                type="text" 
+                value= {address.street || ''}
+                onChange = {(event) => {
+                dispatch({type: 'SET_STREET', payload: event.target.value})
+                }}
+                />
                 
-                <label> Apartment, suite, etc.
-                  <input 
+                <p>Apartment, suite, etc.</p>
+                  <TextField 
+                  variant="outlined"
                   type="text" 
                   value= {address.apartment || ''}
                   onChange = {(event) => {
                   dispatch({type: 'SET_APARTMENT', payload: event.target.value})
                   }}
                   />
-                </label>
-                <label> City
-                  <input 
+
+
+                <p>City</p>
+                  <TextField 
+                  variant="outlined"
                   type="text" 
                   value= {address.city || ''}
                   onChange = {(event) => {
                     dispatch({type: 'SET_CITY', payload: event.target.value})
                   }}
                   />
-                </label>
-                <label> State
-                  <input 
+
+
+                <p>State</p>
+                  <TextField 
+                  variant="outlined"
                   type="text" 
                   value= {address.state || ''}
                   onChange = {(event) => {
                     dispatch({type: 'SET_STATE', payload: event.target.value})
                   }}
                   />
-                </label>
-                <label> Zip/postal code
-                  <input 
+
+                <p>Zip/Postal code</p>
+                  <TextField 
+                  variant="outlined"
                   type="text" 
                   value= {address.zip || ''}
                   onChange = {(event) => {
                     dispatch({type: 'SET_ZIP', payload: event.target.value})
                   }}
                   />
-                </label>
-              </Stack>
-              <div className="btnContainer">
-                  <div className="nextBtn">
-                      <Button 
-                          type="submit" 
-                          size= "large"
-                          sx={{
-                              backgroundColor: '#CE8077',
-                              color: '#f8f8f8',
-                              margin: '2%',
-                              paddingTop: '16px', paddingBottom: '16px',
-                              paddingRight: '32px', paddingLeft: '32px'
-                          }}
-                        >
-                              Next
-                        </Button>  
-                    </div>
-                    <div className="cancelBtn">
-                      <Button onClick={cancelBtn}
-                          type="submit" 
-                          size= "large"
-                          sx={{
-                              backgroundColor: '#CE8077',
-                              color: '#f8f8f8',
-                              margin: '2%',
-                              paddingTop: '16px', paddingBottom: '16px',
-                              paddingRight: '32px', paddingLeft: '32px'
-                          }}
-                          >
-                              Cancel
-                        </Button> 
-                    </div>
-                </div>
-            </form>
-          </div>
+              </div>     
+            </Stack>
+          </form>
+        </div>
+        <div className="btnContainer">
+            <div className="nextBtn">
+                <Button 
+                    onClick={nextBtn} 
+                    size= "large"
+                    sx={{
+                        backgroundColor: '#CE8077',
+                        color: '#f8f8f8',
+                        margin: '2%',
+                        paddingTop: '16px', paddingBottom: '16px',
+                        paddingRight: '32px', paddingLeft: '32px'
+                    }}
+                  >
+                        Next
+                  </Button>  
+              </div>
+              <div className="cancelBtn">
+                <Button onClick={cancelBtn}
+                    type="submit" 
+                    size= "large"
+                    sx={{
+                        backgroundColor: '#CE8077',
+                        color: '#f8f8f8',
+                        margin: '2%',
+                        paddingTop: '16px', paddingBottom: '16px',
+                        paddingRight: '32px', paddingLeft: '32px'
+                    }}
+                    >
+                        Cancel
+                  </Button> 
+              </div>
+        </div>
       </>     
     )
 }
