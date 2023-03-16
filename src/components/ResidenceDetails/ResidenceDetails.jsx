60a51b51ade36188acd95680b6bdd6e8cae35b4c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-03-16 11:04:36 -0500

    property details page

--- src/components/ResidenceDetails/ResidenceDetails.jsx
+++ src/components/ResidenceDetails/ResidenceDetails.jsx
@@ -25,61 +25,106 @@ function ResidenceDetails(residences) {
         history.push(`/propertygallery`)
 }
 
-  return(
-    <>
-    <Card sx={{
-        border: "none", 
-        boxShadow: "none",
-        backgroundColor: '#FFF',
-        color: '#111856',
-        maxWidth: 800,
-        marginTop: 6,
-        marginBottom: 6,
-        marginLeft: 8,
-        marginRight: 8,
-        padding: 3,
-        lineHeight: 1,
-        textAlign: 'left',
-        borderRadius: 6
-
-    }}
-    >
-        <Card sx={{
-            textAlign: 'left',
-            fontSize: 25,
-            backgroundColor: '#FFF',
-            color: '#410064',
-            lineHeight: 1,
-            fontFamily: 'Rugrats Sans',
-            padding: 1.5,
-            lineHeight: 1,
-            textAlign: 'left',
-            borderRadius: 4,
-            border: "none", 
-        boxShadow: "none"
-        }}>{propertyReducer.residence.propertyName}</Card >
-        <br></br>
-        <Card sx={{
-            textAlign: 'left',
-            fontSize: 22,
-            backgroundColor: '#FFFFFF',
-            color: '#410064',
-            lineHeight: 1,
-            fontFamily: 'Rugrats Sans',
-            padding: 1.5,
-            lineHeight: 1,
-            textAlign: 'left',
-            borderRadius: 4,
-            marginBottom: 2,
-            border: "none", 
-        boxShadow: "none"
-        }}>
+    return(
+        <>
         
-            <p>House Type: {propertyReducer.residence.houseType}</p>
-            <p>{propertyReducer.residence.propertyName}</p>
-            <p>Description: {propertyReducer.residence.description}</p>
+            <Card sx={{
+                border: "none", 
+                boxShadow: "none",
+                backgroundColor: '#FFF',
+                color: '#111856',
+                maxWidth: 800,
+                marginTop: 6,
+                marginBottom: 6,
+                marginLeft: 8,
+                marginRight: 8,
+                padding: 3,
+                lineHeight: 1,
+                textAlign: 'left',
+                borderRadius: 6
+
+                    }}
+                    >
+                        <Card sx={{
+                            textAlign: 'left',
+                            fontSize: 18,
+                            backgroundColor: '#FFF',
+                            color: '#410064',
+                            lineHeight: 1,
+                            width: 400,
+                            padding: 1.5,
+                            lineHeight: 1,
+                            textAlign: 'left',
+                            borderRadius: 4,
+                            border: "none", 
+                        boxShadow: "none"
+                        }}><h1>{propertyReducer.residence.propertyName}</h1>
+                            {propertyReducer.residence.featurePhoto}
+                            </Card >
+                        <br></br>
+                        <Card sx={{
+                            textAlign: 'left',
+                            fontSize: 22,
+                            backgroundColor: '#FFFFFF',
+                            color: '#410064',
+                            lineHeight: 1,
+                            width: 450,
+                            padding: 1.5,
+                            lineHeight: 1,
+                            textAlign: 'left',
+                            borderRadius: 4,
+                            marginBottom: 2,
+                            border: "none", 
+                        boxShadow: "none"
+                        }}>
+                        
+                            <p>Maximun Guests: {propertyReducer.residence.maxGuests}</p>
+                            <p>{propertyReducer.residence.propertyName}</p>
+                            <p>{propertyReducer.residence.description}</p>
+                            </Card>
+
+                            <Card sx={{
+                            textAlign: 'center',
+                            fontSize: 22,
+                            backgroundColor: '#FFFFFF',
+                            color: '#410064',
+                            lineHeight: 1,
+                            width: 400,
+                            padding: 1.5,
+                            lineHeight: 1,
+                            textAlign: 'left',
+                            borderRadius: 4,
+                            marginBottom: 2,
+                            border: "none", 
+                        boxShadow: "none"
+                        }}>
+                                <p>Bedrooms: {propertyReducer.residence.bedrooms}</p> 
+                                <p> Bathrooms: {propertyReducer.residence.bathrooms} Beds: {propertyReducer.residence.beds}</p>
+                                
+                            </Card>
+                            </Card>
+                        <Card sx={{
+                            textAlign: 'left',
+                            fontSize: 18,
+                            backgroundColor: '#FFFFFF',
+                            color: '#410064',
+                            lineHeight: 1,
+                            width: 400,
+                            padding: 1.5,
+                            lineHeight: 1,
+                            textAlign: 'left',
+                            borderRadius: 4,
+                            marginTop: -85,
+                            marginLeft: 100,
+                            marginBottom: 100,
+                            
+                        }}>
+                            <h1>About this stay</h1>
+                            <p>Minimum Stay Length: {propertyReducer.residence.minStayLength}</p>
+                            <p>Daily Price: {propertyReducer.residence.priceDaily} Monthly Price {propertyReducer.residence.priceMonthly}</p>
+
+                        </Card>
             
-            </Card>
             
 
     <Button sx={{
@@ -88,8 +133,7 @@ function ResidenceDetails(residences) {
         backgroundColor: '#D1877F',
         marginLeft: 4
     }} onClick={() => backToProperties(residences)}>Back</Button>
-        </Card>
-    </>
+        </>
 )
 
 }
