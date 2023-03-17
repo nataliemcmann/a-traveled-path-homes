491484adcbf6dbf03d2be5394e27fb95df36f013
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-03-17 10:51:18 -0500

    add property and map work

--- src/components/UserDashboard/UserToggle.jsx
+++ src/components/UserDashboard/UserToggle.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Switch, Grid } from '@mui/material'
+import { Switch, Grid, Card } from '@mui/material'
 
 
 
